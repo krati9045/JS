a13321fa77b8db4836f2c6e6d89417266af38733
krati9045 <118352082+krati9045@users.noreply.github.com>
2023-11-21 09:22:05 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+//alert(3+3)  // we are using node Js not browser 
+
+let name="Krati"
+let age = 18
+let isLoggedIn = false
+
+// PRIMITIVE DATA TYPES
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => T/F
+// null -> standalone value (representation of empty value; can be defined also)
+// undefined => when value is not assigned
+// symbol => to find uniqueness  [unique]
+
+// Object
+
+console.log(typeof "krati")
+console.log(typeof age)
+console.log(typeof null)        //object
+console.log(typeof undefined)   //undefined
+
+
+
